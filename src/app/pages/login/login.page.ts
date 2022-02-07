c5860ca151ea26fa53a1f1d54f2b6aea4e1c0753
Abhishek Parmar <parmarsanjay4369@gmail.com>
2022-02-07 15:08:43 +0530

    TODO sanjay: commit after work on the api

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { MenuController } from '@ionic/angular';
-import { TranslateService } from '@ngx-translate/core';
-import { ApiService } from 'src/app/services/api.service';
-import { UtilService } from 'src/app/services/util.service';
-import { message, session } from 'src/app/utility/message';
-import { pattern } from 'src/app/utility/pattern';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { MenuController } from "@ionic/angular";
+import { TranslateService } from "@ngx-translate/core";
+import { ApiService } from "src/app/services/api.service";
+import { UtilService } from "src/app/services/util.service";
+import { message, session } from "src/app/utility/message";
+import { pattern } from "src/app/utility/pattern";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
 export class LoginPage implements OnInit {
   form: FormGroup;
@@ -28,74 +28,77 @@ export class LoginPage implements OnInit {
     private formBuilder: FormBuilder,
     private apiService: ApiService
   ) {}
-  type = 'password';
+  type = "password";
   ionViewDidEnter() {
-    
     this.menuCtrl.enable(false);
-   
   }
   initForm() {
     this.form = this.formBuilder.group({
       email: [
-        '',
+        "",
         [
           Validators.required,
           Validators.pattern(pattern.email),
           Validators.maxLength(100),
         ],
       ],
-      password: ['', [Validators.required, Validators.maxLength(100)]],
+      password: ["", [Validators.required, Validators.maxLength(100)]],
     });
 
     this.validationMessage = {
       email: [
-        { type: 'required', message: message.required },
-        { type: 'pattern', message: message.email },
-        { type: 'maxlength', message: message.maxLength(100) },
+        { type: "required", message: message.required },
+        { type: "pattern", message: message.email },
+        { type: "maxlength", message: message.maxLength(100) },
       ],
       password: [
-        { type: 'required', message: message.required },
-        { type: 'pattern', message: message.password },
-        { type: 'maxlength', message: message.maxLength(100) },
+        { type: "required", message: message.required },
+        { type: "pattern", message: message.password },
+        { type: "maxlength", message: message.maxLength(100) },
       ],
     };
   }
   ngOnInit() {
-     this.initForm();
+    this.initForm();
     this.menuCtrl.enable(false);
   }
 
   changeType() {
-    if (this.type == 'password') {
-      this.type = 'text';
+    if (this.type == "password") {
+      this.type = "text";
     } else {
-      this.type = 'password';
+      this.type = "password";
     }
   }
-
+  signup() {
+    this.router.navigate(["/register"]);
+  }
   async login() {
-     this.submitted = true;
+    this.submitted = true;
     if (this.form.valid && this.utility.checkNetwork()) {
       this.submitted = false;
       let params: any = this.form.getRawValue();
       let loading = await this.utility.presentLoading();
-      this.apiService.loginUser(params).then((res:any) => {
-        loading.dismiss();
-        if (res.status == 1) {
-          let token = res.jwt;
-          let data = res.data;
-          this.utility.setStorage(session.AUTH_STATUS, 1);
-          this.utility.setStorage(session.AUTH_TOKEN, token);
-          this.utility.setStorage(session.AUTH_USER, JSON.stringify(data));
-          this.router.navigate(['/area-of-interest']);
-        } else {
-          this.utility.presentToast(res.msg);
-        }
-      }).catch((err: any) => {
-        console.log(err);
-        loading.dismiss();
-      })
+      this.apiService
+        .loginUser(params)
+        .then((res: any) => {
+          loading.dismiss();
+          if (res.status == 1) {
+            let token = res.jwt;
+            let data = res.data;
+            this.utility.setStorage(session.AUTH_STATUS, 1);
+            this.utility.setStorage(session.AUTH_TOKEN, token);
+            this.utility.setStorage(session.AUTH_USER, JSON.stringify(data));
+            this.router.navigate(["/area-of-interest"]);
+          } else {
+            this.utility.presentToast(res.msg);
+          }
+        })
+        .catch((err: any) => {
+          console.log(err);
+          loading.dismiss();
+        });
       console.log(params);
-    } 
+    }
   }
 }
--- src/app/utility/message.ts
+++ src/app/utility/message.ts
@@ -1,46 +1,46 @@
 export var message = {
-  deleteConfirm: 'Are you sure that you want to delete this record?',
-  activeConfirm: 'Are you sure that you want to active this record?',
-  inactiveConfirm: 'Are you sure that you want to active this record?',
-  required: 'This field is required',
-  email: 'Please enter valid email address',
-  facebook: 'Please enter valid facebook url',
-  twitter: 'Please enter valid twitter url',
-  instagram: 'Please enter valid instagram url',
-  linkedin: 'Please enter valid linkedin url',
-  youtube: 'Please enter valid youtube url',
-  pintrest: 'Please enter valid pintrest url', 
-  phoneNumber: 'Please enter valid phone number.',
-  alphabate: 'Please enter only alphabate.',
-  alphaNumeric: 'Please enter only alphanumeric.',
-  numeric: 'Please enter only numeric.',
+  deleteConfirm: "Are you sure that you want to delete this record?",
+  activeConfirm: "Are you sure that you want to active this record?",
+  inactiveConfirm: "Are you sure that you want to inactive this record?",
+  required: "This field is required",
+  email: "Please enter valid email address",
+  facebook: "Please enter valid facebook url",
+  twitter: "Please enter valid twitter url",
+  instagram: "Please enter valid instagram url",
+  linkedin: "Please enter valid linkedin url",
+  youtube: "Please enter valid youtube url",
+  pintrest: "Please enter valid pintrest url",
+  phoneNumber: "Please enter valid phone number.",
+  alphabate: "Please enter only alphabate.",
+  alphaNumeric: "Please enter only alphanumeric.",
+  numeric: "Please enter only numeric.",
   password:
-    'Please define a password that meets the following criteria: <br>1] Atleast 8 characters in total. <br>2] Atleast 1 special charater. <br>3] Atleast 1 number. <br>4] Atleast 1 upper case alphabet. <br>5] Atleast 1 lower case alphabet.',
-  confirmPassword: 'Password & Confirm Password must be same.',
-  acceptTerms: 'Please accpet terms and conditions.',
-  changePassword: 'Password changed successfully.',
-  forgetPassword: 'Please check your email.',
-  onlyImageType: 'Only allow jpg, jpeg, gif & png.',
+    "Please define a password that meets the following criteria: <br>1] Atleast 8 characters in total. <br>2] Atleast 1 special charater. <br>3] Atleast 1 number. <br>4] Atleast 1 upper case alphabet. <br>5] Atleast 1 lower case alphabet.",
+  confirmPassword: "Password & Confirm Password must be same.",
+  acceptTerms: "Please accpet terms and conditions.",
+  changePassword: "Password changed successfully.",
+  forgetPassword: "Please check your email.",
+  onlyImageType: "Only allow jpg, jpeg, gif & png.",
   maxLength: maxLength,
   minLength: minLength,
   fixLength: fixLength,
 };
 
 export function maxLength(number: number) {
-  return 'You cannot enter more than ' + number + ' characters';
+  return "You cannot enter more than " + number + " characters";
 }
 
 export function minLength(number: number) {
-  return 'This must contain at least ' + number + ' characters';
+  return "This must contain at least " + number + " characters";
 }
 
 export function fixLength(number: number) {
-  return 'This must contain ' + number + ' characters';
+  return "This must contain " + number + " characters";
 }
 
 export var session = {
-  AUTH_TOKEN: 'AUTH_TOKEN',
-  AUTH_STATUS: 'AUTH_STATUS',
-  AUTH_USER: 'AUTH_USER',
-    BOOKING: 'AUTH_BOOKING',
+  AUTH_TOKEN: "AUTH_TOKEN",
+  AUTH_STATUS: "AUTH_STATUS",
+  AUTH_USER: "AUTH_USER",
+  BOOKING: "AUTH_BOOKING",
 };
